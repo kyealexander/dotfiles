48f197e318dd8a3753330eda8aae57c942103665
Kye Murphy <kyealexandermurphy@gmail.com>
2025-04-04 13:07:29 +1000

    Visual sync: Polybar alignment, picom blur, and wallpaper backup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,3 +3,8 @@ if status is-interactive
 end
 
 set -U fish_greeting
+
+alias ls="exa -al --color=always --group-directories-first"
+alias ll="exa -l --color=always --group-directories-first"
+alias la="exa -a --color=always --group-directories-first"
+alias lt="exa -lTa --color=always --group-directories-first"
--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-# killall -q polybar
-
-# Wait until the processes have been shut down 
+killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar on all monitors
+LEFT_PADDING=10
+RIGHT_PADDING=10
+
+SCREEN_WIDTH=$(xrandr | grep '*' | awk '{print $1}' | cut -d 'x' -f 1)
+
+BAR_WIDTH=$(( SCREEN_WIDTH - LEFT_PADDING - RIGHT_PADDING ))
+OFFSET_X=$(( LEFT_PADDING ))
+
+export BAR_WIDTH
+export OFFSET_X
+
 polybar example &
